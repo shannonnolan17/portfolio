ebbe004abd007d4905217495c396a3fd02fb59ae
Shannon Nolan <shannonnolan17@gmail.com>
2018-06-17 12:10:31 -0700

    Update font...again

--- src/components/Title.js
+++ src/components/Title.js
@@ -5,8 +5,7 @@ class Title extends Component {
   render() {
     return (
       <div className="title">
-        <h1 className="title">Hello, my name is Shannon. I am a Developer living in San Francisco, CA</h1>
-
+        <h1 className="title">Hello, my name is Shannon. I am a Developer living in San Francisco, CA.</h1>
       </div>
     );
   }
