90b769c6c20a0f90a82b95c59b320cf0381dfbd8
Shannon Nolan <shannonnolan17@gmail.com>
2018-06-17 12:55:16 -0700

    Adjust about me size

--- src/components/About.js
+++ src/components/About.js
@@ -4,7 +4,7 @@ import '../About.css';
 class About extends Component {
   render() {
     return (
-      <div className="about">
+      <div className="inner-container">
         <h4 className="title-font">About</h4>
         <p className="p-font">I am a former teacher turned techie that loves all things web development. I noticed that I always loved bringing technology into the classroom and that my favorite days were the ones that I could play around with the latest educational technology tools. So in June of 2017 I decided I wanted to do a coding bootcamp after talking with a couple friends who went through similar programs. In my research, I came across DevBootcamp and instantly knew that was where I wanted to go. I applied, got in, and traveled to San Francisco in order to complete the program in November of 2017. As the last cohort of DevBootcamp, I had the unique opportunity to stay for a couple extra weeks and complete two final projects. It has been a whirlwind experience but definitely the best decision of my life.</p>
       </div>
