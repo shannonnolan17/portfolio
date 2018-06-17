88b7e5360879b12ed881251031e7aeaf141778d7
Shannon Nolan <shannonnolan17@gmail.com>
2018-06-17 11:47:08 -0700

    Add border

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,14 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
 
-class App extends Component {
+class Navbar extends Component {
   render() {
     return (
       <div className="App">
-        <h1>HELLO</h1>
+
       </div>
     );
   }
 }
 
-export default App;
+export default Navbar;
