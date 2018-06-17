a7b3757be63afa831770775d4ff636d3562d5121
Shannon Nolan <shannonnolan17@gmail.com>
2018-06-17 13:25:50 -0700

    Add projects

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,7 +6,7 @@ class Navbar extends Component {
       <div className="Navbar">
         <nav class="navbar navbar-light bg-light">
           <a class="navbar-brand" href="#">
-            <img src="/assets/brand/bootstrap-solid.svg" width="30" height="30" alt=""/> Shannon Nolan
+            <img src="/assets/brand/bootstrap-solid.svg" width="30" height="30" alt=""/>
           </a>
         </nav>
       </div>
