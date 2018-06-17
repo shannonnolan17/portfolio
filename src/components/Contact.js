72a456a47ec9bfc9cb30b3982f729d534fb1bd69
Shannon Nolan <shannonnolan17@gmail.com>
2018-06-17 15:17:35 -0700

    Center button

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -7,6 +7,9 @@ class Contact extends Component {
     return (
       <div className="container">
         <h1 className="title-font-contact">Want me to build you something or want to contact me?</h1>
+        <div className="button-wrapper">
+          <a href="mailto:smnolan7@gmail.com" className="email-button">Email Me <span class="arrow-right">→</span></a>
+        </div>
       </div>
     );
   }
