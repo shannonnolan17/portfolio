57a7249b9092c51e2bd4dd361a097783abd562f4
Shannon Nolan <shannonnolan17@gmail.com>
2018-06-17 12:08:00 -0700

    Update font for title

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import React, { Component } from 'react';
 import Navbar from './components/Navbar';
+import Title from './components/Title';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Navbar />
+        <section className="container">
+          <Navbar />
+        </section>
+
+        <section className="container">
+          <Title />
+        </section>
       </div>
     );
   }
---
+++ src/components/Title.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import '../Title.css';
+
+class Title extends Component {
+  render() {
+    return (
+      <div className="title">
+        <h1 className="title">Hello, my name is Shannon. I am a Developer living in San Francisco, CA</h1>
+
+      </div>
+    );
+  }
+}
+
+export default Title;
