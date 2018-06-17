3450c2e35b2c4ee5fe8f2b78acf796736871132a
Shannon Nolan <shannonnolan17@gmail.com>
2018-06-17 13:35:00 -0700

    Add image

--- src/components/About.js
+++ src/components/About.js
@@ -9,8 +9,14 @@ class About extends Component {
         <p className="p-font">I am a former elementary school teacher turned techie that noticed that I always loved bringing technology into the classroom. So I packed my things and headed to San Francisco to attend DevBootcamp where my passion for coding expanded. I am currently doing part-time work with SenYours(an app to connect seniors to their community) and I am a Salesforce Guru at the Columbus Organization. You can check out my latest work history on my <a href="https://www.linkedin.com/in/shannonnolan/" target="_blank" className="link-color">Linkedin</a>. When I'm not learning something new, I love to hike and take pictures or work on my many side projects found on my <a href="https://github.com/shannonnolan17" target="_blank" className="link-color">GitHub</a>. Currently, I am looking for a full-stack(or frontend/ backend) role in San Francisco.</p>
 
         <h3 className="title-font">Projects</h3>
-        <h4 className='subtitle'>Forever Aloaner</h4>
-        <p className="p-font">Forever Aloaner is a full rails web application that allows users to interact, chat, loan, and borrow items from one another.</p>
+        <h4 className='subtitle'>FOREVER ALOANER</h4>
+        <p className="p-font">This <a href="https://foreveraloaner.herokuapp.com/" target="_blank" className="link-color">project</a> is a full rails web application that allows users to interact, chat, loan, and borrow items from one another. You can<a href="https://github.com/shannonnolan17/foreveraloaner-rails" target="_blank" className="link-color"> check it out</a> on GitHub!</p>
+        <img className="container about-pic" src={require('./landing-page.png')} alt="Photo by Thomas Verbruggen on Unsplash"/>
+
+        <h3 className="title-font">Projects</h3>
+        <h4 className='subtitle'>GOOD GRIEF</h4>
+        <p className="p-font">This <a href="https://foreveraloaner.herokuapp.com/" target="_blank" className="link-color">project</a> is a full rails web application that allows users to interact, chat, loan, and borrow items from one another. You can<a href="https://github.com/shannonnolan17/foreveraloaner-rails" target="_blank" className="link-color"> check it out</a> on GitHub!</p>
+        <img className="container about-pic" src={require('./landing-page.png')} alt="Photo by Thomas Verbruggen on Unsplash"/>
       </div>
     );
   }
