0fed14eda41da33ade6d246401de17c8714f752e
Shannon Nolan <shannonnolan17@gmail.com>
2018-06-18 19:59:05 -0700

    Position logo

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,7 +5,7 @@ class Navbar extends Component {
   render() {
     return (
       <div className="navbar-section">
-        <nav class="navbar navbar-light bg-light">
+        <nav class="">
           <a className="logo-position" href="#">
             <img src={require("./Shannon's logo 3.png")} width="80" height="65" alt=""/>
           </a>
